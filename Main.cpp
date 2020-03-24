0e5f8324bc0319f6ed8c2f936f4232d8116dd80c
Илья Колесников <58778310+ShaldonS@users.noreply.github.com>
2020-03-24 16:10:33 +0300

    Update Main.cpp
    Подключил необходимые библиотеки и добавил операции с векторами.

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,31 @@
+#include <iostream>
+#include <vector>
+#include "vector.h"
+#include "matrix.h"
+using namespace std;
+
 int main()
 {
-  cout << "Hello";
+  //векторы
+	int Number = 5;
+	Vector vec1(2, 1, 5), vec2(3, 2, 1), Result(0,0,0);
+	cout << "Vectors: " << endl;
+	cout << "First vector: ";
+	cout << vec1; //вывод с помощью оператора <<
+	cout << endl;
+	cout << "Second vector: ";
+	vec2.Input();
+	cout << endl << "Multiplication: ";
+	(vec1 * vec2).Input();
+	cout << endl << "Multiplication(by a number 5) 1: ";
+	(vec1 * Number).Input();
+	cout << endl << "Multiplication(by a number 5) 2: ";
+	(vec2 * Number).Input();
+	cout << endl << "Add: ";
+	(vec1 + vec2).Input();
+	cout << endl << "Substraction: ";
+	Result = vec1 - vec2;
+	Result.Input();
+	cout << endl;
+	cout << endl;
 }
