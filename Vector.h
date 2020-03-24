92807eac9e834722fe614ccce124e0e7c62dc062
Илья Колесников <58778310+ShaldonS@users.noreply.github.com>
2020-03-24 12:20:14 +0300

    Update Vector.h

--- Vector.h
+++ Vector.h
@@ -1,5 +1,15 @@
-class Vector
-{
-public: 
-    cout<<"Hello World!";
+class Vector {
+public:
+	Vector(int x = 0, int y = 0, int z = 0) : a(x), b(y), c(z) {};
+	Vector operator*(const Vector&);//умножение на вектор
+	Vector operator*(const int&);//умножение на число
+	Vector operator+(const Vector&tmp);
+	Vector operator-(const Vector&tmp);
+	Vector operator=(const Vector&tmp);
+	Vector operator<<(const Vector&tmp);
+	friend ostream& operator<<(ostream& out, const Vector& tmp);
+	void Input();
+private:
+	int* vect;
+	int a, b, c;
 };
